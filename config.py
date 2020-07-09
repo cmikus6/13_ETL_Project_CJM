8bc920652736943320c9bcc942439e5896ea0222
unknown <cmikus6@gmail.com>
2020-07-09 16:20:32 -0400

    Edits

--- config.py
+++
@@ -1,9 +0,0 @@
-sf_username = 'cmikus6@gmail.com'
-sf_password = 'Wkx+h9Cw1'
-sf_security_token = 'Z9Im92LdnOLPzHgGT5UtQg8f'
-
-remote_db_endpoint = 'database-1.cqhiwxkbskhg.us-east-2.rds.amazonaws.com'
-remote_db_port = '3306'
-remote_db_user = 'cmikus6'
-remote_db_pwd = 'Wkx+h9Cw1'
-remote_db_name = 'gwsis'
